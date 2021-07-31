936af3e36c44da5918369a26ab96bc7b3336ca27
Mazzzy <shaikhmazahar@gmail.com>
2021-07-31 16:24:59 +0530

    added the build folder

---
+++ build/precache-manifest.06b4657f33424bfa0e7d451c807638ad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec864cda6702736c946c80877ca59d9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5b4ce22261a2ef3b190e",
+    "url": "./static/css/2.78063165.chunk.css"
+  },
+  {
+    "revision": "c792cbb0474ed6625e8f",
+    "url": "./static/css/main.50d239b5.chunk.css"
+  },
+  {
+    "revision": "5b4ce22261a2ef3b190e",
+    "url": "./static/js/2.509124c5.chunk.js"
+  },
+  {
+    "revision": "d6b29286b0bbae6e17db84419d44fb6a",
+    "url": "./static/js/2.509124c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c792cbb0474ed6625e8f",
+    "url": "./static/js/main.de305b47.chunk.js"
+  },
+  {
+    "revision": "0bb4eb37e09b80c79523",
+    "url": "./static/js/runtime-main.7f568536.js"
+  },
+  {
+    "revision": "7d352eef4fc0d24048db63b8a4644f4e",
+    "url": "./static/media/logo.7d352eef.svg"
+  },
+  {
+    "revision": "568758605e0b7e73ae1470cee89799f0",
+    "url": "./static/media/logo_white.56875860.svg"
+  }
+]);
